6652a82f59774989969269cd0b0e89335a2dc942
AyushiBokhre <aayushibokhre04@gmail.com>
2026-08-05 19:05:43 +0530

    done

--- Java_IOStream/src/com/rays/iostream/TestBufferedWriter.java
+++ Java_IOStream/src/com/rays/iostream/TestBufferedWriter.java
@@ -6,15 +6,34 @@ import java.io.IOException;
 
 public class TestBufferedWriter {
 	public static void main(String[] args) throws IOException {
+<<<<<<< HEAD
+<<<<<<< HEAD
 		BufferedWriter file = new BufferedWriter(
 				new FileWriter("C:\\Users\\lenovo\\OneDrive\\Desktop\\IOFiles\\buffer.txt"));
 
+=======
+		BufferedWriter file=new BufferedWriter(new FileWriter("C:\\Users\\lenovo\\OneDrive\\Desktop\\IOFiles\\buffer.txt"));
+		
+>>>>>>> 73caf71 (done)
+=======
+		BufferedWriter file = new BufferedWriter(
+				new FileWriter("C:\\Users\\lenovo\\OneDrive\\Desktop\\IOFiles\\buffer.txt"));
+
+>>>>>>> 1e71671 (done)
 		file.write("Hello Everyone!");
 		file.newLine();
 		file.write("I am Ayushi");
 		System.out.println("write successfully");
 		file.close();
+<<<<<<< HEAD
+<<<<<<< HEAD
 
+=======
+		
+>>>>>>> 73caf71 (done)
+=======
+
+>>>>>>> 1e71671 (done)
 	}
 
 }
